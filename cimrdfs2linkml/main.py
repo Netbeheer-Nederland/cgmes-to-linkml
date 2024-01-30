49b4d3147af2a79c5e4e19b775e12e243485380a
Bart Kleijngeld <bartkl@gmail.com>
2024-01-30 16:08:04 +0100

    Added CLI

--- cimrdfs2linkml/main.py
+++ cimrdfs2linkml/main.py
@@ -1,22 +1,28 @@
 from pathlib import Path
 
+import click
 import yaml
 
 from cimrdfs2linkml.cimrdfs.parse import parse_profile
 from cimrdfs2linkml.linkml.generate import generate_schema
 
-if __name__ == "__main__":
-    root_dir = Path(".")
-    eq_prof_fp = root_dir / "data" / "IEC61970-600-2_CGMES_3_0_0_RDFS2020_DY.rdf"
-    # eq_prof_fp = root_dir / "data" / "IEC61970-600-2_CGMES_3_0_0_RDFS2020_EQ.rdf"
-    namespaces, ontology_meta, profile_meta, classes, enums = parse_profile(eq_prof_fp)
+
+@click.command()
+@click.argument("profile", type=click.Path(exists=True, path_type=Path))
+@click.option("--output", "-o", default=Path("out.yaml"), type=click.Path(path_type=Path), help="Output LinkML schema file path.")
+def cli(profile, output):
+    namespaces, ontology_meta, profile_meta, classes, enums = parse_profile(profile)
     linkml_schema = generate_schema(namespaces, ontology_meta, profile_meta, classes, enums)
     # pprint(linkml_schema)
 
-    with open("out.yaml", mode="w") as f:
+    with open(output, mode="w") as f:
         f.write(
             yaml.safe_dump(
                 linkml_schema.model_dump(mode="json", exclude_unset=True, exclude_none=True),
                 sort_keys=False,
             )
         )
+    
+
+if __name__ == "__main__":
+    cli.main()
